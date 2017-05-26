8665d6cc17819b70e022b299ccc3b60cfd33ae8c
Cédric L. Charlier <cedric.charlier@yucom.be>
2017-05-26 16:06:08 +0200

    Initial setup of the repository

---
+++ package-Idunn.ps1
@@ -0,0 +1,25 @@
+$root = (split-path -parent $MyInvocation.MyCommand.Definition)
+$lib = "$root\.package\lib\45\"
+if (Test-Path $lib)
+{
+	Remove-Item $lib -recurse
+}
+new-item -Path $lib -ItemType directory
+new-item -Path $root\.nupkg -ItemType directory -force
+Copy-Item $root\Idunn\bin\Debug\* $lib
+
+$version = $env:GitVersion_NuGetVersion
+if ([string]::IsNullOrEmpty($version))
+{
+    Write-Warning "No version found in environment variables, using version of the dll"
+    $version = (Get-Item $lib\Idunn.exe).VersionInfo.FileVersion
+}
+Write-Host "Setting .nuspec version tag to $version"
+
+$content = (Get-Content $root\Idunn.nuspec -Encoding UTF8) 
+$content = $content -replace '\$version\$',$version
+
+$content | Out-File $root\.package\Idunn.compiled.nuspec -Encoding UTF8
+
+& NuGet.exe pack $root\.package\Idunn.compiled.nuspec -Version $version -OutputDirectory $root\.nupkg
+
